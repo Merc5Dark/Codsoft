dc42c4a6c01385cc0f75af13a0ecca6560c26c8a
Merc5Dark <31131794siya@gmail.com>
2023-11-14 15:24:11 +0200

    set up

---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+// npm init - y
+// npm i express mongoose body-parser cors nodemon
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connect required + set it up
+
+// Routes 
+// Variable declaration
+const tasksRouter = require('./routes/tasks'); 
+const projectsRouter = require('./routes/projects'); 
+
+app.use('./tasks', tasksRouter);
+app.use('./projects', projectsRouter);
+
+app.listen(PORT, () => {
+    console.log('Server is running on port: ${PORT}');
+});
+
